35491dbd226c349aed3046357ead1174d3b70ca2
jjoylee <ekdnlt1995@gmail.com>
2018-06-19 13:29:37 +0900

    Customizing the Django Admin

--- instagramclone/images/models.py
+++ instagramclone/images/models.py
@@ -18,6 +18,9 @@ class Image(TimeStampedModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
 class Comment(TimeStampedModel):
 
      """Comment Model"""
@@ -25,8 +28,14 @@ class Comment(TimeStampedModel):
      creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
      image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
 
+     def __str__(self):
+         return self.message
+
 class Like(TimeStampedModel):
     
     """Like Model"""
     creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'User:{} - Image Caption:{}'.format(self.creator.username, self.image.caption)
