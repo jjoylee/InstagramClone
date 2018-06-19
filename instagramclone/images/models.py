abce4b5ccb53c9cb5e023a028c391230bbc6bada
jjoylee <ekdnlt1995@gmail.com>
2018-06-19 12:54:46 +0900

    Created Like Model and fixing Comment and Image models

--- instagramclone/images/models.py
+++ instagramclone/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from instagramclone.users import models as user_models
 
 # Create your models here.
 class TimeStampedModel(models.Model):
@@ -11,10 +12,21 @@ class TimeStampedModel(models.Model):
         
 class Image(TimeStampedModel):
 
+    """Image Model"""
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
 
 class Comment(TimeStampedModel):
 
-    message = models.TextField()
+     """Comment Model"""
+     message = models.TextField()
+     creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+     image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
+
+class Like(TimeStampedModel):
+    
+    """Like Model"""
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
