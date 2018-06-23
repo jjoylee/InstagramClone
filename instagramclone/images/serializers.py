cf4ffbf2a1600bbf8b77cb5a58b9bf7bfcfff1e2
jjoylee <ekdnlt1995@gmail.com>
2018-06-23 14:48:00 +0900

    getting followers list

--- instagramclone/images/serializers.py
+++ instagramclone/images/serializers.py
@@ -41,3 +41,12 @@ class ImageSerializer(serializers.ModelSerializer):
             'creator'
         )
 
+class UserProfileImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Image
+        fields = (
+            'id',
+            'file',
+            'comment_count',
+            'like_count',
+        )
