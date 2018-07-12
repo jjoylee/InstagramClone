b714996e3ded92e5e313f4f06cd073d54dd94de7
jjoylee <ekdnlt1995@gmail.com>
2018-07-12 12:30:41 +0900

    Redux Middlewares : Logger

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -2,7 +2,15 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import users from 'redux/modules/users';
 
-const middlewares = [thunk]
+const middlewares = [thunk];
+
+const env = process.env.NODE_ENV; // prod or development environment
+
+if(env === "development"){
+    // devlopment 환경에서만 redux-logger 부르기 not in prod environment
+    const { logger } = require('redux-logger');
+    middlewares.push(logger);
+}
 
 const reducer = combineReducers({
     users
